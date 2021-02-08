a2301d49104010fd2516e183cb2a22bb3f465289
Istiaque1994 <istiqueahmed80@gmail.com>
2021-02-08 17:24:44 +0600

    some

---
+++ app.js
@@ -0,0 +1,15 @@
+const searchItems = () => {
+    const searchText = document.getElementById("search-field").value;
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
+    // console.log(url);
+
+    //Data Load
+    fetch(url)
+    .then(res => res.json())
+    .then (data => displayItems(meals.meals))
+    
+}
+
+const displayItems = items => {
+    console.log(items);
+}
